53e1ff8bd38f7cc1487c241dd92c6be37e387256
ZoeRichter <zoerichter96@gmail.com>
2022-04-14 16:56:31 -0500

    added axis labels and timestep argument

--- scripts/decay.py
+++ scripts/decay.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plot
 
-def plotdecay(initial_amount, half_life, time):
+def plotdecay(initial_amount, half_life, time, timesteps):
     """
     Plots the decay of an isotope, if given the half-life, decay, and initial
     number of atoms
@@ -15,10 +15,11 @@ def plotdecay(initial_amount, half_life, time):
     none
     """
 
-    time_steps = np.linspace(0,time, time)
+    time_steps = np.linspace(0,time, timesteps)
 
     isotope_atom = initial_amount*np.exp(-half_life*time_steps)
-
+    plot.ylabel("Number of Atoms")
+    plot.xlabel("Time [s]")
     plot.plot(time_steps,isotope_atom)
 
     return
